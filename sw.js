3d43cd27a92e39608668ffe14ccb2c55a680acda
KurKuza <47026225+KurKuza@users.noreply.github.com>
2022-04-29 23:43:29 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const staticCacheName = 's-app-v1'
+
+const assetUrls = [
+	'index.html',
+	'/js/app.js',
+	'/css/style.css',
+]
+
+
+self.addEventListener('install', async event => {
+	const cache = await caches.open(staticCacheName)
+	await cache.addAll(assetUrls)
+})
+
+self.addEventListener('activate', (event) => {
+	console.log('[SW]: activate')
+})
+
+self.addEventListener('fetch', event => {
+	console.log('Fetch', event.request.url)
+
+	event.respondWith(cacheFirst(event.request))
+})
+
+
+async function cacheFirst(request) {
+	console.log(caches.match(request))
+}
